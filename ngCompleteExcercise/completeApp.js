06206fa666ff6244a6a30abdd4456b7428e578da
Kanis Nealon <kanis.nealon@gmail.com>
2015-05-09 18:34:03 -0400

    Adding Initial Files
    Setting up repo.

---
+++ ngCompleteExcercise/completeApp.js
@@ -0,0 +1,1 @@
+﻿var completeApp = angular.module("completeApp", ["ngAutocomplete"]);
---
+++ ngCompleteExcercise/completeController.js
@@ -0,0 +1,31 @@
+﻿completeApp.controller("completeController", function ($scope) {
+
+    //$scope.api = ngAutocomplete;
+
+    $scope.update = function(details)
+    {
+        $scope.info = details.address_components;
+        $scope.members = details.address_components.length;
+
+        $scope.address = {};
+
+        for(i = 0; i < $scope.members; i++)
+        {
+            if ($scope.info[i].types[0] == "street_number")
+                $scope.address.number = $scope.info[i].long_name;
+
+            if ($scope.info[i].types[0] == "route")
+                $scope.address.street = $scope.info[i].long_name;
+
+            if ($scope.info[i].types[0] == "locality")
+                $scope.address.city = $scope.info[i].long_name;
+
+            if ($scope.info[i].types[0] == "administrative_area_level_1")
+                $scope.address.state = $scope.info[i].long_name;
+
+            if ($scope.info[i].types[0] == "postal_code")
+                $scope.address.zip = $scope.info[i].long_name;
+        }
+    }
+
+});
